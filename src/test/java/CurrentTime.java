1f58835cfd958d485d5c4fe697e1b34497e3fa63
seeyoui <seeyouiken@163.com>
2015-07-22 15:00:45 +0800

    清空test目录

--- src/test/java/CurrentTime.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-
-
-public class CurrentTime {
-	public static void main(String[] args) {
-		System.out.println(new Date().getTime());
-	}
-}
--- src/test/java/Test.java
+++
@@ -1,6 +0,0 @@
-
-public class Test {
-public static void say(String something) {
-	System.out.println(something);
-}
-}
