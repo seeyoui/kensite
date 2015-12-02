c11a6ac4a5eb9f832f946486cfdeb85f0092a34f
seeyoui <seeyouiken@163.com>
2015-12-02 09:03:27 +0800

    代码生成器模版调整

--- template_kensite_V1/${projectName}/src/main/java/${basepackage_dir}/${innerpackage_dir}/${classNameFirstLower}/controller/${className}Controller.java
+++ template_kensite_V1/${projectName}/src/main/java/${basepackage_dir}/${innerpackage_dir}/${classNameFirstLower}/controller/${className}Controller.java
@@ -60,6 +60,22 @@ public class ${className}Controller extends BaseController {
 	}
 	
 	/**
+	 * 根据ID查询单条数据
+	 * @param modelMap
+	 * @param module
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/data/{id}")
+	@ResponseBody
+	public Object data(HttpSession session,
+			HttpServletResponse response, HttpServletRequest request,
+			ModelMap modelMap, @PathVariable String id) throws Exception {
+		${className} ${table.classNameFirstLower} = ${table.classNameFirstLower}Service.findOne(id);
+		return ${table.classNameFirstLower};
+	}
+	
+	/**
 	 * 获取列表展示数据
 	 * @param modelMap
 	 * @param ${table.classNameFirstLower}
@@ -71,7 +87,7 @@ public class ${className}Controller extends BaseController {
 	@ResponseBody
 	public Object listData(HttpSession session,
 			HttpServletResponse response, HttpServletRequest request,
-			ModelMap modelMap, ${className} ${table.classNameFirstLower}) throws Exception{
+			ModelMap modelMap, ${className} ${table.classNameFirstLower}) throws Exception {
 		List<${className}> ${table.classNameFirstLower}List = ${table.classNameFirstLower}Service.findList(${table.classNameFirstLower});
 		int total = ${table.classNameFirstLower}Service.findTotal(${table.classNameFirstLower});
 		EasyUIDataGrid eudg = new EasyUIDataGrid();
@@ -92,7 +108,7 @@ public class ${className}Controller extends BaseController {
 	@ResponseBody
 	public Object listAll(HttpSession session,
 			HttpServletResponse response, HttpServletRequest request,
-			ModelMap modelMap, ${className} ${table.classNameFirstLower}) throws Exception{
+			ModelMap modelMap, ${className} ${table.classNameFirstLower}) throws Exception {
 		List<${className}> ${table.classNameFirstLower}List = ${table.classNameFirstLower}Service.findAll(${table.classNameFirstLower});
 		return ${table.classNameFirstLower}List;
 	}
@@ -109,7 +125,7 @@ public class ${className}Controller extends BaseController {
 	@ResponseBody
 	public String save(HttpSession session,
 			HttpServletResponse response, HttpServletRequest request,
-			ModelMap modelMap, ${className} ${table.classNameFirstLower}) throws Exception{
+			ModelMap modelMap, ${className} ${table.classNameFirstLower}) throws Exception {
 		if (!beanValidator(modelMap, ${table.classNameFirstLower})){
 			RequestResponseUtil.putResponseStr(session, response, request, modelMap, StringConstant.FALSE);
 			return null;
@@ -131,7 +147,7 @@ public class ${className}Controller extends BaseController {
 	@ResponseBody
 	public String update(HttpSession session,
 			HttpServletResponse response, HttpServletRequest request,
-			ModelMap modelMap, ${className} ${table.classNameFirstLower}) throws Exception{
+			ModelMap modelMap, ${className} ${table.classNameFirstLower}) throws Exception {
 		if (!beanValidator(modelMap, ${table.classNameFirstLower})){
 			RequestResponseUtil.putResponseStr(session, response, request, modelMap, StringConstant.FALSE);
 			return null;
@@ -171,7 +187,7 @@ public class ${className}Controller extends BaseController {
 	@RequestMapping(value = "/export")
 	public String export(HttpSession session,
 			HttpServletResponse response, HttpServletRequest request,
-			ModelMap modelMap, TestW testW) throws Exception{
+			ModelMap modelMap, ${className} ${table.classNameFirstLower}) throws Exception {
 		String fileName = DateUtils.getDate("yyyyMMddHHmmss")+".xlsx";
 		List<${className}> ${table.classNameFirstLower}List = ${table.classNameFirstLower}Service.findAll(${table.classNameFirstLower});
 		new ExportExcel(null, ${className}.class).setDataList(${table.classNameFirstLower}List).write(response, fileName).dispose();
