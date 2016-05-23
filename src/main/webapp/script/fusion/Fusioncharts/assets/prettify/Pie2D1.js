e010e9bc91e2ba60740cc8fec096131caa2ed015
seeyoui <seeyouiken@163.com>
2016-05-23 15:47:26 +0800

    移除fusion相关flash图表插件，占空太大了

--- src/main/webapp/script/fusion/Fusioncharts/assets/prettify/Pie2D1.js
+++
@@ -1,6 +0,0 @@
-var dataString ='<chart caption="各型号统计&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" showPercentageInLabel="1" showValues="1" showLabels="0" showLegend="1" showPercentValues="1">\n\
-	<set value="14.94" label="蝶影" color="429EAD"/> \n\
-	<set value="19.17" label="雅俪" color="4249AD"/> \n\
-	<set value="7.14" label="雅致六代" color="AD42A2"/> \n\
-	<set value="7.75" label="雅魅" color="D4AC31"/>\n\
-</chart>';
